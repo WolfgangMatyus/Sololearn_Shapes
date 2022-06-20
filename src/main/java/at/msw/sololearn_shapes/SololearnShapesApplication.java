b1d5cfcee040224a83bc8f45804fb40dd608c35f
WolfgangMatyus <wi21b055@technikum-wien.at>
2022-06-21 00:41:36 +0200

    initial Commit

---
+++ src/main/java/at/msw/sololearn_shapes/SololearnShapesApplication.java
@@ -0,0 +1,13 @@
+package at.msw.sololearn_shapes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SololearnShapesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SololearnShapesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/msw/sololearn_shapes/SololearnShapesApplicationTests.java
@@ -0,0 +1,13 @@
+package at.msw.sololearn_shapes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SololearnShapesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
