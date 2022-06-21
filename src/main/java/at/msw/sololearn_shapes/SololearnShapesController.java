b11b34fb3197a83e31bbb1ddc5970b97b1305f48
Wolfgang Matyus <105013150+WolfgangMatyus@users.noreply.github.com>
2022-06-21 03:55:19 +0200

    route for SqareArea and CircleArea implemented (#10)

--- src/main/java/at/msw/sololearn_shapes/SololearnShapesController.java
+++ src/main/java/at/msw/sololearn_shapes/SololearnShapesController.java
@@ -3,20 +3,25 @@ package at.msw.sololearn_shapes;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // .json -> sucht nach json files
 // @Controller // .html -> sucht nach html files
 public class SololearnShapesController {
-
     /*
     @RequestMapping() // GET POST DELETE PATCh PUT
     @DeleteMapping()
     @PostMapping()
     */
-    @GetMapping("/") // GET
-    public String index(){
-        return "hello";
+    @GetMapping("/squareArea") // GET
+    public int squareArea(@RequestParam int a){
+        return a * a;
+    }
+
+    @GetMapping("/circleArea") // GET
+    public double circleArea(@RequestParam int a){
+        return Math.PI * a * a;
     }
 
 }
