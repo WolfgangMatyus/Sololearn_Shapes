9e973cfd855483ff11fde5523f15ef5024399e64
WolfgangMatyus <wi21b055@technikum-wien.at>
2022-06-21 02:21:33 +0200

    implemented Shape Class and subclasses Square and Circle

---
+++ src/main/java/at/msw/sololearn_shapes/Shape.java
@@ -0,0 +1,16 @@
+package at.msw.sololearn_shapes;
+
+public abstract class Shape {
+
+    private double width;
+
+    public abstract double area();
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+}
